b1947df514e1c650beb1d6b1274dc939928dc937
rend1027 <rend1027@yahoo.com>
2025-06-14 17:00:49 -0400

    Test 10 complete

--- 10-sumOfNumsWithinARange/sumOfNumsWithinARange.js
+++ 10-sumOfNumsWithinARange/sumOfNumsWithinARange.js
@@ -1,5 +1,13 @@
 function sumOfNumsWithinARange(nums, start, end) {
   // Insert code here;
+  let counter = 0;
+
+  for(let element of nums) {
+    if(element >= start && element <= end) {
+      counter ++;
+    }
+  }
+  return counter;
 }
 
 // Do not edit this line;
