bced0e7273a6af33640c69ef488bcf65364f7d13
rend1027 <rend1027@yahoo.com>
2025-06-14 15:58:09 -0400

    Test 06 complete

--- 06-countOfAllNumbersSmallerThanTarget/countOfAllNumbersSmallerThanTarget.js
+++ 06-countOfAllNumbersSmallerThanTarget/countOfAllNumbersSmallerThanTarget.js
@@ -1,5 +1,12 @@
 function countOfAllNumbersSmallerThanTarget(nums, target) {
   // Insert code here;
+  let counter = 0;
+  for(let element of nums) {
+    if( element < target) {
+      counter ++;
+    }
+  }
+  return counter;
 }
 
 // Do not edit this line;
