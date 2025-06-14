a053704d398b0e551ebbd4481b5b3b3436b70e66
rend1027 <rend1027@yahoo.com>
2025-06-14 16:52:01 -0400

    Test 08 complete

--- 08-sumOfAllEvenNumbers/sumOfAllEvenNumbers.js
+++ 08-sumOfAllEvenNumbers/sumOfAllEvenNumbers.js
@@ -1,5 +1,12 @@
 function sumOfAllEvenNumbers(nums) {
   // Insert code here;
+  let counter = 0;
+  for(let element of nums) {
+    if(element % 2 == 0) {
+      counter ++;
+    }
+  }
+  return counter;
 }
 
 // Do not edit this line;
