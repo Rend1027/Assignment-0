7f810a7a6b0a06fa99c5bcec8be496a3b8705a67
rend1027 <rend1027@yahoo.com>
2025-06-14 15:49:12 -0400

    Test 05 complete

--- 05-countOfAllBooleansAndStrings/countOfAllBooleansAndStrings.js
+++ 05-countOfAllBooleansAndStrings/countOfAllBooleansAndStrings.js
@@ -1,5 +1,18 @@
 function countOfAllBooleansAndStrings(arr) {
   // Insert code here;
+  let numOfbool = 0;
+  let numOfstr = 0;
+
+  for(let element of arr) {
+    if(typeof element == "string") {
+      numOfstr ++;
+    } else {
+      if(typeof element == "boolean") {
+        numOfbool ++;
+      }
+    }
+  }
+  return numOfbool + numOfstr;
 }
 
 // Do not edit this line;
