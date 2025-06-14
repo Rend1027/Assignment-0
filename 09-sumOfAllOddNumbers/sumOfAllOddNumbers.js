2ec80077bdab9e4026ad543270ba227e284c6731
rend1027 <rend1027@yahoo.com>
2025-06-14 16:55:01 -0400

    Test 09 complete

--- 09-sumOfAllOddNumbers/sumOfAllOddNumbers.js
+++ 09-sumOfAllOddNumbers/sumOfAllOddNumbers.js
@@ -1,5 +1,13 @@
 function sumOfAllOddNumbers(nums) {
   // Insert code here;
+  let counter = 0;
+
+  for(let element of nums) {
+    if(element % 2 != 0) {
+      counter ++;
+    }
+  }
+  return counter;
 }
 
 // Do not edit this line;
