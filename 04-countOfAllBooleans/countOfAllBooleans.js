4a8b75b292d430669181cadea053bac9178560c9
rend1027 <rend1027@yahoo.com>
2025-06-14 14:56:25 -0400

    Test 04 complete

--- 04-countOfAllBooleans/countOfAllBooleans.js
+++ 04-countOfAllBooleans/countOfAllBooleans.js
@@ -1,5 +1,12 @@
 function countOfAllBooleans(arr) {
   // Insert code here;
+  let numOfBool = 0;
+  for (let element of arr) {
+    if(typeof element == "boolean") {
+      numOfBool++
+    }
+  }
+  return numOfBool;
 }
 
 // Do not edit this line;
