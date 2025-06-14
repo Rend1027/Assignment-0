74cfd8888229589362e776df0b9b7c9bdfa07bc9
rend1027 <rend1027@yahoo.com>
2025-06-14 19:43:41 -0400

    Test 13 complete

--- 13-isPalindrome/isPalindrome.js
+++ 13-isPalindrome/isPalindrome.js
@@ -1,5 +1,13 @@
 function isPalindrome(word) {
   // Insert code here;
+  //
+  console.log(word,">>>>>This is the word")
+  for(let start = 0, end = word.length - 1; start < end ; start++, end--) {
+    if(word[start] != word[end]) {
+      return false;
+    }
+  }
+  return true;
 }
 
 // Do not edit this line;
