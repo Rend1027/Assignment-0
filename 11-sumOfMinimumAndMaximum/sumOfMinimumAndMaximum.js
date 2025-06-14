e703c7077613a19c8b5c8092718b48906b35656e
rend1027 <rend1027@yahoo.com>
2025-06-14 17:57:54 -0400

    Test 11 complete

--- 11-sumOfMinimumAndMaximum/sumOfMinimumAndMaximum.js
+++ 11-sumOfMinimumAndMaximum/sumOfMinimumAndMaximum.js
@@ -1,5 +1,20 @@
 function sumOfMinimumAndMaximum(nums) {
   // Insert code here;
+  let minValue = nums[0];
+  let maxValue = nums[0];
+  let sum = 0;
+
+  for(let i = 0; i < nums.length; i++) {  //[2,1,5,3,23]
+    if (minValue > nums[i]) {
+      minValue = nums[i];
+    } 
+    if(maxValue < nums[i]){
+      maxValue = nums[i];
+    }
+  }
+  sum = minValue + maxValue;
+  return sum;
+  
 }
 
 // Do not edit this line;
