4283f2db9a39901d8ea473d4496d90f7b64c6fdd
rend1027 <rend1027@yahoo.com>
2025-06-14 14:29:03 -0400

    Test 01 complete

--- 01-titleCaseEdit/titleCaseEdit.js
+++ 01-titleCaseEdit/titleCaseEdit.js
@@ -1,13 +1,26 @@
 function titleCaseEdit(title) {
   // Insert code here;
-  let toLowerCase = title.toLowerCase();
-  console.log(toLowerCase);
-  toLowerCase = title.split(" ");
-  // console.log(toLowerCase[1]);
-  for(string of toLowerCase) {
-    string[0].toUpperCase;
-  }
-  return toLowerCase.join(" ")
+  // initialize a variable to buld up-- empty string.
+  //title is a string I need assess to the first element of each string.
+  //use split using space as the seperator
+  // Now we have a an array where each word is an element
+  // Now i need to access the first index of each element
+  // we can use a loop but what kind?
+  // use for of loop to iteriate thru the array.
+  // but we also have to iterate thru each element[0].
+  // then we have to convert the first elemnt of each word to upperCase
+  // then we have to push the upperCase element to the empty string
+  // we also have to push the rest of the word.
+  // return build up variable.
+
+
+  let strArr = title.split(" ");
+
+ let tempArray = strArr.map(word => word[0].toUpperCase() + word.slice(1));
+
+ return tempArray.join(" ");
+
+
 }
 
 // Do not edit this line;
