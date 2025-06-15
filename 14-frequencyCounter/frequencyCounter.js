b431f667dfe4ea98e64814e9cf420fa7d73415f9
rend1027 <rend1027@yahoo.com>
2025-06-14 21:30:40 -0400

    Test 14 complete

--- 14-frequencyCounter/frequencyCounter.js
+++ 14-frequencyCounter/frequencyCounter.js
@@ -1,5 +1,13 @@
 function frequencyCounter(word) {
   // Insert code here;
+  let obj = {};
+
+  for(let element of word) {
+    if(obj[element]) {
+      obj[element]++;
+    } else (obj[element] = 1)
+  }
+  return obj;
 }
 
 // Do not edit this line;
