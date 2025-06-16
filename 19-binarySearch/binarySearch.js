eb6b42cd6a396e9915a32ec639cee2594b8f62f1
rend1027 <rend1027@yahoo.com>
2025-06-16 10:51:02 -0400

    Test 19 almost complete

--- 19-binarySearch/binarySearch.js
+++ 19-binarySearch/binarySearch.js
@@ -5,6 +5,30 @@ class MySolution {
 fdfasdf
   binarySearch(nums, target) {
     // Insert code here;
+    //         target----> 2
+    //           [1  ,   2,   3,   4,   5,   6]
+    //            0      1    2    3    4    5
+    //  index--> start       mid            end  
+
+    let foundTarget = false;
+    let start = 0;
+    let end = nums.length - 1;
+    
+
+    while(start <= end) {
+      let mid = Math.floor(((end + start)/ 2));
+    if( nums[mid] === target ) {
+      return true;
+    }
+    if(nums[mid] > target) {
+       end = mid - 1;
+       
+    } else {
+      start = mid + 1;
+      
+    }
+    }
+    return false
   }
 }
 
