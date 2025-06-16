7ebd583cf66151363005c35c83f7e50850b87295
rend1027 <rend1027@yahoo.com>
2025-06-16 08:38:26 -0400

    test 18 complete

--- 18-pairSum/pairSum.js
+++ 18-pairSum/pairSum.js
@@ -1,19 +1,16 @@
 function pairSum(nums, target) { //[5, 3, 1] targer--> 6
   // Insert code here;
-
-  console.log(nums)
-  console.log("this is target>>>",target)
-
   if(nums.length <= 1) {
-    throw new Error("error");
+    throw new error("battery assist")
   }
   for(let i = 0; i < nums.length; i++) {
-    console.log("this is result--->", nums[i] + nums[i + 1])     
-    if(nums[i] + nums[i + 1] === target) {
-      return true;
-    } 
-     return false;
-  }
+    for(let j = 1; i < nums.length; i++) {
+      if(nums[i] + nums[j] === target) {
+        return true;
+      }
+    }
+  } return false;
+  
 }
 
 // Do not edit this line;
