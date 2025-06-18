fa70d8219ed3172fb4f38b4f387d73e83c19a06b
rend1027 <rend1027@yahoo.com>
2025-06-18 18:19:40 -0400

    ban recursive

--- 19-binarySearch/binarySearch.js
+++ 19-binarySearch/binarySearch.js
@@ -2,36 +2,45 @@ class MySolution {
   constructor() {
     this.flag = false; // this is optional to use;
   }
-fdfasdf
-  binarySearch(nums, target) {
+  binarySearch(nums, target, start=0, end=nums.length - 1) {
     // Insert code here;
-    //         target----> 2
-    //           [1  ,   2,   3,   4,   5,   6]
-    //            0      1    2    3    4    5
-    //  index--> start       mid            end  
+    //                       target----> 1
+    //                   [1  ,    2,     3,   4,   5,   6]
+    // index-->           0       1      2    3    4    5
+    //                   start   mid                 
+    //                    end           
+    //                    
 
-    let foundTarget = false;
-    let start = 0;
-    let end = nums.length - 1;
-    
+    if(end < start ) {
+      return false
+    }
+    let mid = Math.floor((start + end) / 2);
 
-    while(start <= end) {
-      let mid = Math.floor(((end + start)/ 2));
-    if( nums[mid] === target ) {
+    if(nums[mid] === target) {
       return true;
     }
     if(nums[mid] > target) {
-       end = mid - 1;
-       
-    } else {
+      end = mid - 1;
+      return this.binarySearch(nums, target, start , end) //
+    }
+    
+    if(nums[mid] < target) {
       start = mid + 1;
-      
+      return this.binarySearch(nums, target, start , end)
     }
-    }
-    return false
+
   }
 }
+//                     start         mid           finish
+// in the first call---> [1  ,   2,   3,   4,   5,   6]
+//                start              end
+// second call---> [3,    4,    5,    6]
+//              start         end
+// third call--> [4,      5,     6]
 
+// fourth call---> [ 5,     6]
+//                start    end
+//                 mid
 // Do not edit this line;
 let StudentSolution = MySolution;
 module.exports = StudentSolution;
